78821e70d49b4643e42c9dc7272b9247b24324bd
Micah Young <micah@young.io>
2012-12-08 10:51:20 -0500

    Make timesheet request also filter by newer-than and older-than dates

--- lib/openair/request/timesheet.rb
+++ lib/openair/request/timesheet.rb
@@ -5,20 +5,17 @@ module OpenAir::Request
       def request(request_options, auth_options, query_options = {})
         login_elements = Login.elements(auth_options)
 
-        read_options = { type: "Timesheet", limit: DEFAULT_LIMIT }
-        if users = query_options[:users]
-          read_options.merge!(method: "user")
-          filter_elements = user_elements(users)
-        else
-          read_options.merge!(method: "all")
-        end
+        options = read_options(query_options)
+        filters = read_filters(query_options)
 
         Nokogiri::XML::Builder.new do |xml|
           xml.request(request_options) {
             xml.Auth { xml.parent << login_elements }
 
-            xml.Read(read_options) {
-              xml.parent << filter_elements if filter_elements
+            xml.Read(options) {
+              filters.each do |filter|
+                xml.parent << filter.elements
+              end
             }
           }
         end.doc
@@ -26,16 +23,73 @@ module OpenAir::Request
 
       private
 
-      def user_elements(users)
+      def read_options(query_options)
+        options = { type: "Timesheet", limit: DEFAULT_LIMIT }
+
+        user_method = query_options[:users] ? "user" : "all"
+        options.merge!(method: user_method)
+
+        newer_than, older_than = query_options.values_at(:newer_than, :older_than)
+        date_filter = if newer_than && older_than
+          {filter: "newer-than,older-than", field: "starts,starts"}
+        elsif newer_than
+          {filter: "newer-than", field: "starts"}
+        elsif older_than
+          {filter: "older-than", field: "starts"}
+        end
+        options.merge!(date_filter) if date_filter
+
+        options
+      end
+
+      def read_filters(query_options)
+        [
+          user_filters_doc(query_options),
+          date_filters_doc(query_options)
+        ].flatten
+      end
+
+      def user_filters_doc(query_options)
+        user_ids = query_options[:users]
+
         Nokogiri::XML::Builder.new do |xml|
           xml.root {
-            Array(users).each do |user_id|
+            Array(user_ids).each do |user_id|
               xml.User {
                 xml.id user_id
               }
             end
           }
-        end.doc.at("root").elements
+        end.doc.at("root")
+      end
+
+      def date_filters_doc(query_options)
+        newer_than, older_than = query_options.values_at(:newer_than, :older_than)
+
+        Nokogiri::XML::Builder.new do |xml|
+          xml.root {
+            if newer_than
+              xml.Date {
+                xml.year newer_than.year
+                xml.month newer_than.month
+                xml.day newer_than.day
+                xml.hour newer_than.hour
+                xml.minute newer_than.min
+                xml.second newer_than.sec
+              }
+            end
+            if older_than
+              xml.Date {
+                xml.year older_than.year
+                xml.month older_than.month
+                xml.day older_than.day
+                xml.hour older_than.hour
+                xml.minute older_than.min
+                xml.second older_than.sec
+              }
+            end
+          }
+        end.doc.at("root")
       end
 
     end
--- spec/lib/openair/request/timesheet_spec.rb
+++ spec/lib/openair/request/timesheet_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "time"
 
 describe OpenAir::Request::Timesheet do
   let(:company_id) { "Abc" }
@@ -35,14 +36,102 @@ describe OpenAir::Request::Timesheet do
       end
     end
 
-    context "with query options to filter users" do
-      let(:query_options) { {users: [111, 999]} }
+    context "with query options" do
+      context "for specific users" do
+        let(:query_options) do
+          {users: [111, 999]}
+        end
 
-      it "builds a timesheet read request with user filter" do
-        subject.should have_request_with_headers_with_key(api_key)
-        subject.should have_request_login
-        subject.css("request > Read[type='Timesheet'][method='user'][limit]").should be_one
-        subject.css("request > Read > User > id").size.should == 2
+        it "builds a request with user filter" do
+          subject.should have_request_with_headers_with_key(api_key)
+          subject.should have_request_login
+          subject.css("request > Read[type='Timesheet'][method='user'][limit]").should be_one
+          subject.css("request > Read > User > id").size.should == 2
+        end
+      end
+
+      context "for a specific date range" do
+        let(:query_options) do
+          {
+            older_than: Time.parse("2011-1-1 8:59:01 UTC"),
+            newer_than: Time.parse("2012-2-29 23:59:58 UTC")
+          }
+        end
+
+        it "builds a request with both date filters" do
+          subject.should have_request_with_headers_with_key(api_key)
+          subject.should have_request_login
+
+          subject.css("request > Read[type='Timesheet'][method='all'][limit][filter='newer-than,older-than'][field='starts,starts']").should be_one
+
+          subject.css("request > Read > Date:nth-child(1)").tap do |date_doc|
+            date_doc.css("year").text.should == "2012"
+            date_doc.css("month").text.should == "2"
+            date_doc.css("day").text.should == "29"
+            date_doc.css("hour").text.should == "23"
+            date_doc.css("minute").text.should == "59"
+            date_doc.css("second").text.should == "58"
+          end
+          subject.css("request > Read > Date:nth-child(2)").tap do |date_doc|
+            date_doc.css("year").text.should == "2011"
+            date_doc.css("month").text.should == "1"
+            date_doc.css("day").text.should == "1"
+            date_doc.css("hour").text.should == "8"
+            date_doc.css("minute").text.should == "59"
+            date_doc.css("second").text.should == "1"
+          end
+        end
+      end
+
+      context "for older-than date only" do
+        let(:query_options) do
+          {
+            older_than: Time.parse("2012-2-1")
+          }
+        end
+
+        it "builds a request with only one date filter" do
+          subject.should have_request_with_headers_with_key(api_key)
+          subject.should have_request_login
+
+          subject.css("request > Read[type='Timesheet'][method='all'][limit][filter='older-than'][field='starts']").should be_one
+          subject.css("request > Read > Date").size.should == 1
+        end
+      end
+
+      context "for newer-than date only" do
+        let(:query_options) do
+          {
+            newer_than: Time.parse("2012-2-1")
+          }
+        end
+
+        it "builds a request with only one date filter" do
+          subject.should have_request_with_headers_with_key(api_key)
+          subject.should have_request_login
+
+          subject.css("request > Read[type='Timesheet'][method='all'][limit][filter='newer-than'][field='starts']").should be_one
+          subject.css("request > Read > Date").size.should == 1
+        end
+      end
+
+      context "to filter by users and a date range" do
+        let(:query_options) do
+          {
+            users: [123, 987],
+            older_than: Time.parse("2012-2-1"),
+            newer_than: Time.parse("2012-1-1")
+          }
+        end
+
+        it "builds a timesheet request with both" do
+          subject.should have_request_with_headers_with_key(api_key)
+          subject.should have_request_login
+
+          subject.css("request > Read[type='Timesheet'][method='user'][limit][filter='newer-than,older-than'][field='starts,starts']").should be_one
+          subject.css("request > Read > User").size.should == 2
+          subject.css("request > Read > Date").size.should == 2
+        end
       end
     end
   end
